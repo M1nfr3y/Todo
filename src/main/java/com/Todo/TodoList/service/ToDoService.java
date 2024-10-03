01f2e617199abf57a198ec85406a777db4e7b932
ale <turrinialessio5@gmail.com>
2024-10-03 17:48:20 +0200

    risolto problema di avvio

--- src/main/java/com/Todo/TodoList/service/ToDoService.java
+++ src/main/java/com/Todo/TodoList/service/ToDoService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.Todo.TodoList.model.Todo;
 import com.Todo.TodoList.repository.ToDoRepository;
 
+@Service
 public class ToDoService {
 
     @Autowired
