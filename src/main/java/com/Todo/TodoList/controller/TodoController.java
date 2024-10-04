d7295668e2c75af2a2f0e589496c7e48f1df0576
ale <turrinialessio5@gmail.com>
2024-10-04 16:51:36 +0200

    implementazione della chiamata create e update

--- src/main/java/com/Todo/TodoList/controller/TodoController.java
+++ src/main/java/com/Todo/TodoList/controller/TodoController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +32,15 @@ public class TodoController {
     @GetMapping("/{id}")
     public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
         return toDoService.getTodoById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo){
+        return toDoService.createToDO(todo);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @RequestBody Todo todo){
+        return ResponseEntity.ok(toDoService.updateToDo(id, todo));
     }
 }
